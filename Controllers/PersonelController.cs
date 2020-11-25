d0583b086edc0c7220c7ff91d6fe46865d1ab618
Efe Özgür <efecuneytozgur@gmail.com>
2020-11-26 01:53:51 +0300

    Initial commit

---
+++ Controllers/PersonelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rehber.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
